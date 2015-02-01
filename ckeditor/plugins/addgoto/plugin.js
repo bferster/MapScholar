dabf1a68a4d791464023ad1a56d4638283162239
Bill Ferster <bferster@virginia.edu>
2015-02-01 12:16:34 -0500

    goto/mob CKEditor plugins

---
+++ ckeditor/plugins/addgoto/plugin.js
@@ -0,0 +1,24 @@
+﻿(function(){									// Code to execute when the toolbar button is pressed
+
+	var a={
+		exec:function(editor){
+	 		var theSelectedText = editor.getSelection().getNative();
+	 		GoToEditor(0,0,function(m) { 
+	 			editor.insertHtml("goto("+m+")")
+	 			},"page");
+			}
+	},
+	b='addgoto';
+	CKEDITOR.plugins.add(b,{					// Create the button and add the functionality to it
+		init:function(editor){
+		editor.addCommand(b,a);
+		editor.ui.addButton("addgoto", {
+	    	label:'Add goto', 
+	    	icon:this.path+"addgoto.png",
+	    	command:b
+	    	});
+		}
+	}); 
+
+})();
+
---
+++ ckeditor/plugins/addmob/plugin.js
@@ -0,0 +1,23 @@
+﻿(function(){									// Code to execute when the toolbar button is pressed
+
+	var a={
+		exec:function(editor){
+	 		var theSelectedText = editor.getSelection().getNative();
+	 		PickMob(function(m) { 
+	 			editor.insertHtml("mob("+m+")")
+	 			},"page");
+		}
+	},
+	b='addmob';
+	CKEDITOR.plugins.add(b,{					// Create the button and add the functionality to it
+		init:function(editor){
+		editor.addCommand(b,a);
+		editor.ui.addButton("addmob", {
+	    	label:'Add mob', 
+	    	icon:this.path+"addmob.png",
+	    	command:b
+	    	});
+		}
+	}); 
+
+})();
