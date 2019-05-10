dbad1d20d511e5f106da76064ab2c30d73646597
Bill Ferster <bferster@virginia.edu>
2019-05-10 14:10:46 -0400

    PHP7

--- geteasyfile.php
+++ geteasyfile.php
@@ -2,20 +2,21 @@
 	header('Cache-Control: no-cache, no-store, must-revalidate'); 
 	header('Expires: Sun, 01 Jul 2005 00:00:00 GMT'); 
 	header('Pragma: no-cache'); 
-	require_once('config.php');
-	$id=mysql_real_escape_string($_GET['id']);
+	require_once('config7.php');
+
+	$id=mysqli_real_escape_string($last,$_GET['id']);
 	if (!is_nan($id)) {
 		$query="SELECT * FROM easyfile WHERE id = '$id'";
-		$result=mysql_query($query);
+		$result=mysqli_query($link, $query);						
 		}
 	else
 		$result=false;
-	mysql_close();
-	if (($result == false) || (!mysql_numrows($result)))
+	if (($result == false) || (!mysqli_num_rows($result)))
 		echo "Can't load file!";
 	else{
-		$type=mysql_result($result,0,"type");
-		$data=mysql_result($result,0,"data");
+		$row=mysqli_fetch_assoc($result);
+		$type=$row["type"];
+		$data=$row["data"];
 		echo "easyFileDataWrapper(\n";	
 		if ($type == "KML") {
 			$data=str_replace("\"","'",$data);	
@@ -33,5 +34,7 @@
 			echo "\"}";
 		echo "\n)";	
 		}
+	mysqli_free_result($result);								// Free
+	mysqli_close($link);										// Close session
 ?>
 
--- getoldkml.php
+++ getoldkml.php
@@ -3,22 +3,26 @@
 	header('Cache-Control: no-cache, no-store, must-revalidate'); 
 	header('Expires: Sun, 01 Jul 2005 00:00:00 GMT'); 
 	header('Pragma: no-cache'); 
-	require_once('config.php');
-	$id=mysql_real_escape_string($_GET['id']);
+	require_once('config7.php');
+
+	$id=mysqli_real_escape_string($link,$_GET['id']);
 	if (!is_nan($id)) {
 		$query="SELECT * FROM easyfile WHERE id = '$id'";
-		$result=mysql_query($query);
+		$result=mysqli_query($link, $query);						
 		}
 	else
 		$result=false;
-	mysql_close();
 	ob_clean();
-	if (($result == false) || (!mysql_numrows($result)))
+	if (($result == false) || (!mysqli_num_rows($result)))
 		echo "Can't load file!";
 	else{
-		$r=mysql_result($result,0,"data");
+		$row=mysqli_fetch_assoc($result);
+		$r=$row["data"];
 		if (!strstr($r,"<?xml"))
 			echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 		echo $r;
 		}
+	mysqli_free_result($result);								// Free
+	mysqli_close($link);										// Close session
+
 ?>
